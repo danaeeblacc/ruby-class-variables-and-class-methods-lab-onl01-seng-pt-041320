03f13a3b7d4b219fd396fbfcc3dfacdef5be1ee6
Teinelle Charles <teinelledcharles@gmail.com>
2020-05-19 02:43:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,24 @@
 class Song
-  
-  attr_accessor :name, :artist, :genre
-  @@count = 0
-  @@artist = []
-  @@genres = []
-  
-  def initialize(song_name,artist,genre)
-    @names =song_name
+
+ attr_accessor :name, :artist, :genre
+ @@count = 0
+ @@artists = []
+ @@genres = []
+
+def initialize(song_name,artist,genre)
+    @name = song_name
     @artist = artist
     @genre = genre
     @@count += 1
     @@artists << artist
     @@genres << genre
-  end
-  
-  def self.count
+end
+
+def self.count 
     @@count
-  end
-  
-  def self.artists
+ end
+ 
+ def self.artists
     @@artists.uniq
   end
   
@@ -26,15 +26,15 @@ class Song
     @@genres.uniq
   end
   
-  def self.genere_count
+  def self.genre_count
     genre_count = Hash.new(0)
     @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
   end
   
   def self.artist_count
-    artist_cout = Hash.new(0)
-    @@artist.each {|artist| artist_count[artist] += 1}
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
     artist_count
   end
 end
